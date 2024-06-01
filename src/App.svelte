37565d66cdf29b78bb16c42ad16974a2c718383d
AyeAreEm <ag.ayeareem@gmail.com>
2024-06-01 15:12:08 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,119 @@
+<script lang="ts">
+let enlarge_dialog: HTMLElement;
+let enlarge_src: string;
+
+let contact_dialog: HTMLElement;
+
+function toggle_enlarge_menu(this: HTMLElement) {
+    enlarge_src = this.getAttribute("src") || "";
+    enlarge_dialog.showModal();
+}
+
+function enlarge_close(event: Event) {
+    if (event.target === enlarge_dialog) {
+        enlarge_dialog.close();
+    }
+}
+
+function contact_close(event: Event) {
+    if (event.target === contact_dialog) {
+        contact_dialog.close();
+    }
+}
+</script>
+
+<main>
+<div class="top_header">
+    <button style="float: left;"><h1>Mansarover</h1></button>
+    <button on:click={contact_dialog.showModal()} style="float: right;" class="contact"><h1>Contact</h1></button>
+</div>
+<img class="halal_logo" src="/src/images/halal_logo.png" alt="mansarover logo">
+
+<div class="menus">
+    <h1>Menus</h1>
+    <img on:click={toggle_enlarge_menu} src="/src/images/mansarover_setlunch.jpg" alt="mansarover setlunch">
+    <img on:click={toggle_enlarge_menu} src="/src/images/mansarover_menu.jpg" alt="mansarover menu">
+</div>
+
+<dialog bind:this={enlarge_dialog} on:click={enlarge_close} id="enlarge_dialog">
+    <img src="{enlarge_src}" alt="menu">
+</dialog>
+
+<dialog bind:this={contact_dialog} on:click={contact_close} id="contact_dialog">
+    <h1>Local number: 2109 1927</h1>
+    <h1>WhatsApp: 6224 9937</h1>
+</dialog>
+</main>
+
+<style>
+.top_header {
+    background: rgb(168,32,55);
+    height: 5vh;
+}
+
+.top_header h1 {
+    font-size: 2rem;
+}
+
+.top_header button {
+    border: none;
+    background: none;
+    margin: 0;
+    padding: 0;
+    /* height: 40px; */
+}
+
+.halal_logo {
+    position: fixed;
+    right: 0;
+    min-width: 100px;
+    width: 7%;
+}
+
+.menus {
+    margin-top: 5%;
+    width: 100%;
+}
+
+.menus img {
+    float: left;
+    width: 40%;
+    height: 70vh;
+    margin-left: 5%;
+    margin-right: 5%;
+}
+
+.menus img:hover {
+    cursor: pointer;
+}
+
+.contact:hover {
+    cursor: pointer;
+}
+
+#enlarge_dialog {
+    margin: auto;
+    height: 100vh;
+    width: 80vw;
+    padding: 0.5rem;
+    border-radius: 0.5em;
+    z-index: 10;
+}
+
+#contact_dialog {
+    margin: auto;
+}
+
+dialog[open] {
+    animation: fade-in 150ms cubic-bezier(0, 0, 0.2, 1);
+}
+
+@media (max-width: 320px) {
+    .menus img {
+        width: 40%;
+        height: 50vh;
+        margin-left: 5%;
+        margin-right: 5%;
+    }
+}
+</style>
